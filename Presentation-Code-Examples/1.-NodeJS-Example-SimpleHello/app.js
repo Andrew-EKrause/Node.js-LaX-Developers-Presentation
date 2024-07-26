bd24711829e6680bf1e5eadef88818904e423b7c
Andrew Krause <57727121+Andrew-EKrause@users.noreply.github.com>
2024-07-26 01:12:22 -0500

    Delete Presentation-Code-Examples directory

--- Presentation-Code-Examples/1.-NodeJS-Example-SimpleHello/app.js
+++
@@ -1,7 +0,0 @@
-// Simple example that we run in the terminal.
-function sayHowDoYaDo(name) {
-    console.log('How do ya do, ' + name + "? Nice day out eh?");
-}
-
-sayHowDoYaDo("THAT Conference"); // Pass in a parameter to execute function. Can run "node app.js" to execute program.
-//console.log(window); // Do not have the window or document objects. These are part of the browser.
