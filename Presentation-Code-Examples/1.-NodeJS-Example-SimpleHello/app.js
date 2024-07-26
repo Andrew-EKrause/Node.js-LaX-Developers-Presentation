9c3e3cb2f54bb7224bb04f19fadd968896a2a79b
Andrew Krause <57727121+Andrew-EKrause@users.noreply.github.com>
2024-07-26 01:19:16 -0500

    Add files via upload

---
+++ Presentation-Code-Examples/1.-NodeJS-Example-SimpleHello/app.js
@@ -0,0 +1,7 @@
+// Simple example that we run in the terminal.
+function sayHowDoYaDo(name) {
+    console.log('How do ya do, ' + name + "? Nice day out eh?");
+}
+
+sayHowDoYaDo("THAT Conference"); // Pass in a parameter to execute function. If Node.js is installed, can run "node app.js" to execute program.
+//console.log(window); // Do not have the window or document objects. These are part of the browser.
